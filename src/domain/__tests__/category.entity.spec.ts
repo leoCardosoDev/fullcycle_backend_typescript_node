a03e20f792fa02c5915611dfa85aa12f7d67c9b0
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-13 18:56:28 -0700

    feat: add category entity

---
+++ src/domain/__tests__/category.entity.spec.ts
@@ -0,0 +1,111 @@
+import { Uuid } from '../../shared/domain/value-objects/uuid_vo'
+import { Category } from '../category.entity'
+
+describe('Category Entity Unit Tests', () => {
+  describe('constructor()', () => {
+    it('should create category only name', () => {
+      const category = new Category({
+        name: 'Movie',
+      })
+      expect(category.category_id).toBeInstanceOf(Uuid)
+      expect(category.name).toBe('Movie')
+      expect(category.description).toBeNull()
+      expect(category.is_active).toBe(true)
+      expect(category.created_at).toBeInstanceOf(Date)
+    })
+
+    it('should create category with all values', () => {
+      const created_at = new Date()
+      const category = new Category({
+        name: 'Movie',
+        description: 'New Description',
+        is_active: false,
+        created_at,
+      })
+      expect(category.category_id).toBeInstanceOf(Uuid)
+      expect(category.name).toBe('Movie')
+      expect(category.description).toBe('New Description')
+      expect(category.is_active).toBe(false)
+      expect(category.created_at).toBe(created_at)
+    })
+
+    it('should create category with name and description', () => {
+      const category = new Category({
+        name: 'Movie',
+        description: 'New Description',
+      })
+      expect(category.category_id).toBeInstanceOf(Uuid)
+      expect(category.name).toBe('Movie')
+      expect(category.description).toBe('New Description')
+      expect(category.is_active).toBe(true)
+      expect(category.created_at).toBeInstanceOf(Date)
+    })
+  })
+
+  describe('create command', () => {
+    it('should create a category', () => {
+      const category = Category.create({
+        name: 'Movie',
+      })
+      expect(category.category_id).toBeInstanceOf(Uuid)
+      expect(category.name).toBe('Movie')
+      expect(category.description).toBeNull()
+      expect(category.is_active).toBe(true)
+      expect(category.created_at).toBeInstanceOf(Date)
+    })
+
+    it('should create a category with description', () => {
+      const category = Category.create({
+        name: 'Movie',
+        description: 'Movie description',
+      })
+      expect(category.category_id).toBeInstanceOf(Uuid)
+      expect(category.name).toBe('Movie')
+      expect(category.description).toBe('Movie description')
+      expect(category.is_active).toBe(true)
+      expect(category.created_at).toBeInstanceOf(Date)
+    })
+
+    it('should create a category with is_active', () => {
+      const category = Category.create({
+        name: 'Movie',
+        description: 'Movie description',
+        is_active: false,
+      })
+      expect(category.category_id).toBeInstanceOf(Uuid)
+      expect(category.name).toBe('Movie')
+      expect(category.description).toBe('Movie description')
+      expect(category.is_active).toBe(false)
+      expect(category.created_at).toBeInstanceOf(Date)
+    })
+  })
+
+  describe('change command', () => {
+    it('should change name', () => {
+      const category = Category.create({
+        name: 'Movie',
+      })
+      category.changeName('Movie 2')
+      expect(category.name).toBe('Movie 2')
+    })
+
+    it('should change description', () => {
+      const category = Category.create({
+        name: 'Movie',
+        description: 'Movie description',
+      })
+      category.changeDescription('Movie description 2')
+      expect(category.description).toBe('Movie description 2')
+    })
+
+    it('should change is_active', () => {
+      const category = Category.create({
+        name: 'Movie',
+        description: 'Movie description',
+        is_active: false,
+      })
+      category.activate()
+      expect(category.is_active).toBe(true)
+    })
+  })
+})
---
+++ src/domain/category.entity.ts
@@ -0,0 +1,61 @@
+import { Uuid } from '../shared/domain/value-objects/uuid_vo'
+
+export type CategoryConstructorProps = {
+  category_id?: Uuid
+  name: string
+  description?: string | null
+  is_active?: boolean
+  created_at?: Date
+}
+
+export type CategoryCreateCommand = {
+  name: string
+  description?: string | null
+  is_active?: boolean
+}
+
+export class Category {
+  category_id: Uuid
+  name: string
+  description: string | null
+  is_active: boolean
+  created_at: Date
+
+  constructor(props: CategoryConstructorProps) {
+    this.category_id = props.category_id ?? new Uuid()
+    this.name = props.name
+    this.description = props.description ?? null
+    this.is_active = props.is_active ?? true
+    this.created_at = props.created_at ?? new Date()
+  }
+
+  static create(props: CategoryConstructorProps): Category {
+    return new Category(props)
+  }
+
+  changeName(name: string): void {
+    this.name = name
+  }
+
+  changeDescription(description: string): void {
+    this.description = description
+  }
+
+  activate() {
+    this.is_active = true
+  }
+
+  desactivate() {
+    this.is_active = false
+  }
+
+  toJSON() {
+    return {
+      category_id: this.category_id.id,
+      name: this.name,
+      description: this.description,
+      is_active: this.is_active,
+      created_at: this.created_at,
+    }
+  }
+}
