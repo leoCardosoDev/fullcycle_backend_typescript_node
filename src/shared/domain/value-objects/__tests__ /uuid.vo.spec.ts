b103402f1d1866a9f4402e862c759c5e219d6d2a
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-16 16:00:33 -0700

    refactor: change uuid

--- src/shared/domain/value-objects/__tests__ /uuid.vo.spec.ts
+++ src/shared/domain/value-objects/__tests__ /uuid.vo.spec.ts
@@ -1,4 +1,4 @@
-import { InvalidUuidError, Uuid } from '../uuid_vo'
+import { InvalidUuidError, Uuid } from '../uuid.vo'
 import { validate as uuidValidate } from 'uuid'
 
 describe('Uuid Unit Tests', () => {
